cff7f30cf24a89ca90de55ceb2009476a1d90cd8
anyexch <446827718@qq.com>
2024-03-13 15:20:03 +0800

    Create background.js
    initial

---
+++ background.js
@@ -0,0 +1,18 @@
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "openMindMap") {
+    chrome.tabs.create({url: "mindmap.html"});
+  }
+});
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.action === "getHistory") {
+        var midnight = new Date();
+        midnight.setHours(0, 0, 0, 0);
+        chrome.history.search({
+            'text': '', // 空字符串返回所有历史记录
+            'startTime': midnight.getTime()
+        }, function(historyItems) {
+            sendResponse({history: historyItems});
+        });
+        return true; // 保持消息通道开放
+    }
+});
